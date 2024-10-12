b2f72c405794f75db0aa7042f860cb102c74ede1
Harshita Surana <67836051+Harshi402@users.noreply.github.com>
2024-10-12 09:54:08 +0000

    ip

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,9 @@ def display_data(record):
 # Streamlit App
 def main():
   total_records = collection.count_documents({})
-  
+  st.sidebar.title("Navigation")
+  st.sidebar.radio("Go to", ["Home", "Data", "About"])
+
   st.title("_GitHub_ :blue[_Good-First-Issues_]")
   #pagination settings
   page_size = 20
